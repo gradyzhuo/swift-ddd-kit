5033022ff838ff35c2c8718c9221c65ac64a9223
Grady Zhuo <gradyzhuo@gmail.com>
2024-06-16 15:04:47 +0800

    [Reverse] to forcly.

--- Sources/EventSourcing/EventSourcingRepository.swift
+++ Sources/EventSourcing/EventSourcingRepository.swift
@@ -9,15 +9,15 @@ public protocol EventSourcingRepository<StorageCoordinator>: Repository {
 
 extension EventSourcingRepository {
     public func find(byId id: AggregateRootType.ID) async throws -> AggregateRootType? {
-        return try await self.find(byId: id, hiddingDeleted: true)
+        return try await self.find(byId: id, forcly: false)
     }
     
-    public func find(byId id: AggregateRootType.ID, hiddingDeleted: Bool) async throws -> AggregateRootType? {
+    public func find(byId id: AggregateRootType.ID, forcly: Bool) async throws -> AggregateRootType? {
         guard var events = try await coordinator.fetchEvents(byId: id) else {
             return nil
         }
 
-        guard hiddingDeleted && (events.contains { $0 is AggregateRootType.DeletedEventType }) else {
+        guard forcly || !(events.contains { $0 is AggregateRootType.DeletedEventType }) else {
             return nil
         }
 
@@ -48,6 +48,7 @@ extension EventSourcingRepository {
 
     public func delete(aggregateRoot: AggregateRootType) async throws {
         try aggregateRoot.markAsDelete()
+
         try await save(aggregateRoot: aggregateRoot)
     }
 }
