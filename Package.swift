c65b70360ad8c276cf71041785e0b21c3e9ae170
Grady Zhuo <gradyzhuo@gmail.com>
2024-07-09 16:16:51 +0800

    Merge pull request #1 from Mendesky/updateTargetEventBus
    [ADD] update target EventBus

--- Package.swift
+++ Package.swift
@@ -33,6 +33,7 @@ let package = Package(
                 "CQRS",
                 "EventSourcing",
                 "KurrentSupport",
+                "EventBus",
                 .product(name: "Logging", package: "swift-log")
             ]
         ),
@@ -56,6 +57,12 @@ let package = Package(
                 "DDDCore",
                 "EventSourcing",
                 .product(name: "EventStoreDB", package: "eventstoredb-swift"),
+            ]
+        ),
+        .target(
+            name: "EventBus",
+            dependencies: [
+                "DDDCore",
             ]
         ),
         .target(
