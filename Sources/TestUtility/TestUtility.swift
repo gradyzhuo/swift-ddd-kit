8b42144048efdbcd43a224f48128f21e407d1708
Grady Zhuo <gradyzhuo@gmail.com>
2024-08-09 10:56:07 +0800

    [ADJUST] aggregateType -> projectableType

--- Sources/TestUtility/TestUtility.swift
+++ Sources/TestUtility/TestUtility.swift
@@ -12,26 +12,14 @@ import Logging
 let logger = Logger(label: "TestUtility")
 
 extension EventStoreDBClient {
-    public func clearStreams<T: AggregateRoot>(aggregateRootType: T.Type, id: T.ID, errorHandler: ((_ error: Error)->Void)? = nil) async {
+    public func clearStreams<T: Projectable>(projectableType: T.Type, id: T.ID, errorHandler: ((_ error: Error)->Void)? = nil) async {
         do{
             let streamIdentifier: Stream.Identifier = .init(name: T.getStreamName(id: id))
             try await self.deleteStream(to: streamIdentifier) { options in
                 options.revision(expected: .streamExists)
             }
         }catch {
-            logger.warning("The error happended when clear stream with \(id) in \(aggregateRootType). error message: \(error)")
-            errorHandler?(error)
-        }
-    }
-
-    public func clearStreams<T: Projectable>(projectableType: T.Type, streamName: String, errorHandler: ((_ error: Error)->Void)? = nil) async {
-        do{
-            let streamIdentifier: Stream.Identifier = .init(name: streamName)
-            try await self.deleteStream(to: streamIdentifier) { options in
-                options.revision(expected: .streamExists)
-            }
-        }catch {
-            logger.warning("The error happended when clear stream with \(streamName) in \(projectableType). error message: \(error)")
+            logger.warning("The error happended when clear stream with \(id) in \(projectableType). error message: \(error)")
             errorHandler?(error)
         }
     }
