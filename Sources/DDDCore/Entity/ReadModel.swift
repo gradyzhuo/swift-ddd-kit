5cd780b05a6199903e7ac96db07afd51b59c24a9
Grady Zhuo <gradyzhuo@gmail.com>
2025-03-31 17:29:26 +0800

    [REVERT] ReadModel to add CreatedEventType

--- Sources/DDDCore/Entity/ReadModel.swift
+++ Sources/DDDCore/Entity/ReadModel.swift
@@ -1,8 +1,21 @@
 public protocol ReadModel: Projectable, Codable {
-    init?(events: [any DomainEvent]) throws
+    associatedtype CreatedEventType: DomainEvent
+
+    init?(first createdEvent: CreatedEventType, other events: [any DomainEvent]) throws
 }
 
 extension ReadModel {
+
+    public init?(events: [any DomainEvent]) throws {
+        var sortedEvents = events.sorted {
+            $0.occurred < $1.occurred
+        }
+        guard let createdEvent = sortedEvents.removeFirst() as? CreatedEventType else {
+            return nil
+        }
+
+        try self.init(first: createdEvent, other: sortedEvents)
+    }
 
     public func restore(event: some DomainEvent) throws {
         try when(happened: event)
