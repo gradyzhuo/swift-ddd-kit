bd86ad8b084f996076718970d413df554ebe99da
Grady Zhuo <gradyzhuo@gmail.com>
2025-07-24 19:05:46 +0800

    [UPDATE] remove get metadata

--- Sources/TestUtility/TestUtility.swift
+++ Sources/TestUtility/TestUtility.swift
@@ -14,11 +14,7 @@ let logger = Logger(label: "TestUtility")
 
 extension KurrentDBClient {
     public func clearStreams<T: Projectable>(projectableType: T.Type, id: T.ID, execpted revision: KurrentDB.StreamRevision = .any, errorHandler: ((_ error: Error)->Void)? = nil) async {
-        let streamName = T.getStreamName(id: id)
-        guard let metadata = try? await self.getStreamMetadata(streamName) else{
-            return
-        }
-        _ = try? await self.deleteStream(streamName){ options in
+        _ = try? await self.deleteStream(T.getStreamName(id: id)){ options in
             options.revision(expected: revision)
         }
     }
